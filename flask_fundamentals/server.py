a762acb1c3e16d92f897aff83d81a2d6f5cdbd8c
MarsX1e <tianyuxie14@gmail.com>
2017-09-10 10:41:33 -0700

    add hidden session in server.py. modify user.py index.py and create hello.py to make it more like a login and create new user page.

--- flask_fundamentals/server.py
+++ flask_fundamentals/server.py
@@ -9,8 +9,12 @@ def create_user():
     print "Got Post Info"
     session['name']=request.form['name']
     session['email']=request.form['email']
+    session['password']=request.form['password']
     print request.form
-    return redirect('/show')
+    if request.form['action']=='create user':
+        return redirect('/show')
+    elif request.form['action']=='login':
+        return redirect('/hello')
     '''
     return render_template('index.html')
     '''
@@ -25,6 +29,14 @@ def show_user():
     # we can write it in render_template or we can also write session in html. check user.html
     # I am concern that what a session. A project? A dictionary? A list? How does it store data? For now we are creating a page for user to read to know their own information, but session is also for creator to check their customer's record. If we use Python OOP to create a class and use it under session, is it a good way to store for creator?
     return render_template('user.html')
+
+
+@app.route('/hello')
+def welcome():
+    return render_template('hello.html',name=session['name'],email=session['email'],password=session['password'])
+'''
+so far it works. I wonder that if I add two if function to check name email and password, will it work as a real website login?.abs
+'''
 app.run(debug=True)
 '''
 how to print request.form and make it show up?
