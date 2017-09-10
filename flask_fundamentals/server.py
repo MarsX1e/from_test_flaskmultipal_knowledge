84c6ae6025359b3ddaa4ba02843b6a9252f17f30
MarsX1e <tianyuxie14@gmail.com>
2017-09-09 20:17:55 -0700

    tried some tricks form flask Templates. get some questions need to be answered

--- flask_fundamentals/server.py
+++ flask_fundamentals/server.py
@@ -2,11 +2,24 @@ from flask import Flask, render_template, request, redirect
 app=Flask(__name__)
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', phrase="hello",times=5)
 @app.route('/users',methods=['POST'])
 def create_user():
     print "Got Post Info"
     name=request.form['name']
     email=request.form['email']
+    print request.form
     return redirect('/')
+    '''
+    return render_template('index.html')
+    '''
+@app.route('/users/<username>')
+def show_user_profile(username):
+    print username
+    return render_template('user.html')
 app.run(debug=True)
+'''
+how to print request.form and make it show up?
+why isn't there any /users page?
+How can I see those print?
+'''
