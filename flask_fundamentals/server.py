326dcb8ba14551465e491d0e9a0e0ee0ee6651f6
MarsX1e <tianyuxie14@gmail.com>
2017-09-10 10:16:29 -0700

    modified server.py and user.html for playing session. question are recorded.

--- flask_fundamentals/server.py
+++ flask_fundamentals/server.py
@@ -1,15 +1,16 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect,session
 app=Flask(__name__)
+app.secret_key='ThisisSecret'
 @app.route('/')
 def index():
     return render_template('index.html', phrase="hello",times=5)
 @app.route('/users',methods=['POST'])
 def create_user():
     print "Got Post Info"
-    name=request.form['name']
-    email=request.form['email']
+    session['name']=request.form['name']
+    session['email']=request.form['email']
     print request.form
-    return redirect('/')
+    return redirect('/show')
     '''
     return render_template('index.html')
     '''
@@ -20,7 +21,10 @@ def show_user_profile(username):
 
 @app.route('/show')
 def show_user():
-    return render_template('user.html',name='Jay',email='kpatel@codingdojo.com')
+    # return render_template('user.html',name=session['name'],email=session['email']) 
+    # we can write it in render_template or we can also write session in html. check user.html
+    # I am concern that what a session. A project? A dictionary? A list? How does it store data? For now we are creating a page for user to read to know their own information, but session is also for creator to check their customer's record. If we use Python OOP to create a class and use it under session, is it a good way to store for creator?
+    return render_template('user.html')
 app.run(debug=True)
 '''
 how to print request.form and make it show up?
